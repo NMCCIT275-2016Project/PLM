dcf5476cac03ed659b3524e81a7047aecc56085e
Safaiaryu <jacquelineradtke@gmail.com>
2016-02-17 00:10:25 -0500

    Jacqueline - Added Views

--- PLM/Controllers/ModulesController.cs
+++ PLM/Controllers/ModulesController.cs
@@ -13,5 +13,58 @@ namespace PLM.Controllers
         {
             return View();
         }
+
+        public ActionResult AddModule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddModule(FormCollection form)
+        {
+            if (form["operation"] == "Add")
+            {
+                return Redirect("~/Modules/AddImage");
+            }
+
+            if (form["operation"] == "Edit")
+            {
+                return Redirect("~/Modules/EditImage");
+            }
+
+            if (form["operation"] == "Remove")
+            {
+                return Redirect("~/Modules/RemoveImage");
+            }
+
+            if (form["operation"] == "Save")
+            {
+                return Redirect("~/Modules/SaveModule");
+            }
+            else
+            {
+                return Redirect("/Modules/AddModule");
+            }   
+        }
+
+        public ActionResult AddImage()
+        {
+            return View();
+        }
+
+        public ActionResult EditImage()
+        {
+            return View();
+        }
+
+        public ActionResult RemoveImage()
+        {
+            return View();
+        }
+
+        public ActionResult SaveModule()
+        {
+            return View();
+        }
     }
 }
